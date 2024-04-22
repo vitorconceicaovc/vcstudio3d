578f352a0b966f999aaf94d8d0205985fa45ed23
Vítor Conceição <vitoralmeidavc@outlook.pt>
2024-04-22 22:42:52 +0100

    base

---
+++ src/components/Description/index.jsx
@@ -0,0 +1,32 @@
+import styles from './style.module.scss';
+import { useInView, motion } from 'framer-motion';
+import { useRef } from 'react';
+import { slideUp, opacity } from './animation';
+import Rounded from '../../common/RoundedButton';
+export default function index() {
+
+    const phrase = "Empowering brands to shine in the digital realm. Let's redefine the standard together. No fluff, just leading-edge solutions.";
+    const description = useRef(null);
+    const isInView = useInView(description)
+    return (
+        <div ref={description} className={styles.description}>
+            <div className={styles.body}>
+                <p>
+                {
+                    phrase.split(" ").map( (word, index) => {
+                        return <span key={index} className={styles.mask}><motion.span variants={slideUp} custom={index} animate={isInView ? "open" : "closed"} key={index}>{word}</motion.span></span>
+                    })
+                }
+                </p>
+                {/*
+                <motion.p variants={opacity} animate={isInView ? "open" : "closed"}>The combination of my passion for design, VR & interaction positions me in a unique place in the architectural visualization and virtual reality rendering.</motion.p>
+                */}
+                <div data-scroll data-scroll-speed={0.1}>
+                    <Rounded className={styles.button}>
+                        <p>About me</p>
+                    </Rounded>
+                </div>
+            </div>
+        </div>
+    )
+}
